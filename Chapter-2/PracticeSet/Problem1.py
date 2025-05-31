5008b90b0469a8d993e0d0c1b3260825defc5fb7
shreesh-pandey <shree25g@gmail.com>
2025-05-31 09:30:14 +0530

    Solved the Q.1: Add two numbers entered by the user.

---
+++ Chapter-2/PracticeSet/Problem1.py
@@ -0,0 +1,18 @@
+# Q.1: Add two numbers entered by the user.
+
+# Let's take two variable a & b.
+# Use 'input()' function to ask user the numbers
+
+a = input("Enter the 1st no.") 
+b = input("Enter the 2nd no.")
+
+# print(a+b) If we did this, the output will be ab (concatinated).
+
+# Ex: If a = 21 & b = 1, print(a+b) will return 211.
+# Reason: Input function considers all inputs as "Strings"
+# To add the numbers, first we will have to convert them to Integers. 
+
+#Convert both to integers
+sum = int(a)+int(b)
+
+print("The sum of the numbers is", sum)
