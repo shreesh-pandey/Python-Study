a345e4dd1afbfac53d585e59f672bda6367a384f
shreesh-pandey <shree25g@gmail.com>
2025-06-05 14:34:58 +0530

    Solved Q.4: Write a program to format the following letter using escape sequence characters.

---
+++ Chapter-3/PracticeSet/3.4 Problem4.py
@@ -0,0 +1,5 @@
+# Q.4: Write a program to format the following letter using escape sequence characters.
+# letter = "Hi, this python course is nice. Thanks!"
+
+letter = "Hi!,\n\tThis python course is nice.\n\t\t\t\t\tThanks!"
+print(letter)
