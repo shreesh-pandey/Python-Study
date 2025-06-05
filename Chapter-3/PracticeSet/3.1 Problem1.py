88655cd5d8b09f789b8465108dd583e3cf8595f4
shreesh-pandey <shree25g@gmail.com>
2025-06-05 13:45:31 +0530

    Solved Q.1: Write a python program to display a user entered name followed by Good Afternoon using input () function.

---
+++ Chapter-3/PracticeSet/3.1 Problem1.py
@@ -0,0 +1,15 @@
+# Q.1: Write a python program to display a user entered name followed by Good Afternoon using input () function.
+
+# We want the program to"
+#   Ask my name.
+#   Then return something like this "Good Afternoon Shreesh"
+
+# We will use input() function to ask the name
+
+name = input("Hi! Tell me your name: ")
+
+print(name) # This will return the name that was the input
+
+# To return "Good Afternoon 'name'", we will print:
+
+print("Good Afternoon", name)
