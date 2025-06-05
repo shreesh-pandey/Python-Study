0c2ae9efa1a831b342ea5d2b55a615e3f942b6a6
shreesh-pandey <shree25g@gmail.com>
2025-06-05 13:44:29 +0530

    Chapter-3 Study Session: Escape Sequence Characters

---
+++ Chapter-3/05_EscapeSequence.py
@@ -0,0 +1,11 @@
+# Escape Sequence Characters
+
+# \n: For new line
+print("Hello\nWorld")       # Output: Hello
+                            #         World                      
+
+# \t: For 'tab'
+print("Name:\tShreesh")     # Output: Name:    Shreesh
+
+# \": For ""
+print("She said: \"Hi\"")   # Output: She said: "Hi"
