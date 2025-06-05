3dce7f1a5b13e387016faed03caf6ea1bca29a30
shreesh-pandey <shree25g@gmail.com>
2025-06-05 13:43:12 +0530

    Chapter-3 Study Session: String Indexing

---
+++ Chapter-3/02_StringIndexing.py
@@ -0,0 +1,13 @@
+# String Indexing
+
+word = "Python"
+
+# Indexing starts with 0. So to print the first letter, we have to use index as 0.
+print(word[0]) # Output will be 'P'
+
+print(word[3]) # Output will be 'h'
+
+# Python also has a concept of Negative Indexing.
+
+print(word[-1])  # Output: 'n'
+print(word[-2])  # Output: 'o'
