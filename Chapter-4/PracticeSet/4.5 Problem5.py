930486902ca6c224edbb3a3006362cb0c42770b2
shreesh-pandey <shree25g@gmail.com>
2025-06-06 20:19:40 +0530

    Solved Q.5: Write a program to count the number of zeros in the following tuple:a = (7, 0, 8, 0, 0, 9)

---
+++ Chapter-4/PracticeSet/4.5 Problem5.py
@@ -0,0 +1,10 @@
+# Q.5: Write a program to count the number of zeros in the following tuple: a = (7, 0, 8, 0, 0, 9)
+
+# We have this tuple.
+a = (7, 0, 8, 0, 0, 9)
+
+# Lets use count() function to count the number of zeros.
+num = a.count(0)
+
+# Lets print the value.
+print(num)
