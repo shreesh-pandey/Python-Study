f86454e9a2b6dfdc0174a99e6233e10de5324c33
shreesh-pandey <shree25g@gmail.com>
2025-06-06 16:25:24 +0530

    Chapter-4 Study Session: Lists in Python

---
+++ Chapter-4/01_list.py
@@ -0,0 +1,28 @@
+
+
+# List of fruits
+fruits = ["apple", "banana", "cherry"]
+
+# List of numbers
+num = [1,2,3,4,5]
+
+# List of names
+names = ["Shreesh", "Pritha", "Mohit"]
+
+# List of floats
+flt = [3.14, 5.16, 1.25]
+
+# List can also contains multiple datatypes in it.
+lst = ["apple", 1, "Shreesh", 3.14]
+
+# Examples of List Operations
+fruits[0]	# Output: Returns "apple"
+num[4] = 7  # Output: Changes 5 to 7.
+len(names)	# Returns number of items in the list
+
+print(fruits[0])
+print(num)
+print(len(names))
+
+# We can check the datatype of the list as well.
+print(type(num))
