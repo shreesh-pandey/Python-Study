76a74b0f541fc2b67867472f4dd7cef249109d7e
shreesh-pandey <shree25g@gmail.com>
2025-06-13 16:29:25 +0530

    Updated the comments in the code.

--- Chapter-5/02_dictionarymethods.py
+++ Chapter-5/02_dictionarymethods.py
@@ -1,19 +1,23 @@
 person = {"name": "Shreesh", "age": 30, "city": "Delhi"}
 
-# To return a list-like view of all the keys.
-print(person.keys())       # dict_keys(['name', 'age', 'city'])
+# Output: To return a list-like view of all the keys.
+print(person.keys())       # Output: dict_keys(['name', 'age', 'city'])
 
-# To return a list-like view of all the values.
-print(person.values())     # dict_values(['Shreesh', 30, 'Delhi'])
+# Output: To return a list-like view of all the values.
+print(person.values())     # Output: dict_values(['Shreesh', 30, 'Delhi'])
 
-# To return the value of the specified key, or None if key not found
-print(person.get("name"))  # Shreesh
-print(person.get("job"))   # None
+# Output: To return the value of the specified key, or None if key not found
+print(person.get("name"))  # Output: Shreesh
+print(person.get("job"))   # Output: None
 
-# To add or update key-value pairs from another dictionary
+# Output: To add or update key-value pairs from another dictionary
 person.update({"age": 31})
-print(person)              # {'name': 'Shreesh', 'age': 31, 'city': 'Delhi'}
+print(person)              # Output: {'name': 'Shreesh', 'age': 31, 'city': 'Delhi'}
 
-# To remove the specified key and returns its value
+# Output: To add key-value pairs from another dictionary
+person.update({"profession": "PM"})
+print(person)              # Output: {'name': 'Shreesh', 'age': 31, 'city': 'Delhi', 'profession': 'PM'}
+
+# Output: To remove the specified key and returns its value
 person.pop("city")
-print(person)              # {'name': 'Shreesh', 'age': 31}
+print(person)              # Output: {'name': 'Shreesh', 'age': 31, 'profession': 'PM'}
