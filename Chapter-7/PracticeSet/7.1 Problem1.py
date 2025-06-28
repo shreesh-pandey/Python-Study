d3817260767cb025e0c4486670cef00aca9d941f
shreesh-pandey <shree25g@gmail.com>
2025-06-28 19:23:42 +0530

    Updated # Q.1

---
+++ Chapter-7/PracticeSet/7.1 Problem1.py
@@ -0,0 +1,24 @@
+# Q.1: Print the multiplication table of a given number from 1 to 10 using a for loop, and then print it in reverse order (from 10 to 1).
+
+# -------------------------- -------------------------- -------------------------- #
+
+# Solution:
+
+# Lets start with asking the user for the number.
+number = int(input("Please enter the number for multiplication table: "))
+
+# Lets make it interactive by asking user if they want print the table normally or in reverse.
+order = input("Do you want to print the table in reverse? (Y/N): ").lower()
+
+# Now, lets print the table.
+print(f"\nThe multiplication table for {number} is:\n")
+
+if order == "y":
+    for i in range (10, 0, -1):
+        print(f"{number} * {i} = {number * i}")
+else:
+    for i in range (1, 11):
+        print(f"{number} * {i} = {number * i}")
+
+
+
