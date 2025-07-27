5da9b7d5805090a37dd88cf1b2a7c18e3e5948bc
shreesh-pandey <shree25g@gmail.com>
2025-07-27 15:56:04 +0530

    Update Q.8 with a better solution to remove the if check.

--- Chapter-7/PracticeSet/7.8 Problem8.py
+++ Chapter-7/PracticeSet/7.8 Problem8.py
@@ -9,3 +9,15 @@ for i in range (1, 101):
 # -------------------------- -------------------------- -------------------------- #
 
 
+# Deep Dive
+
+# We can make this code better like this:
+
+for i in range(2, 101, 2):
+    print(i)
+
+# Why is it better?
+# Because we are directly looping over even numbers, avoiding the unnecessary 'if i % 2 == 0 check'.
+
+
+
