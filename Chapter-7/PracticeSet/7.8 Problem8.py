5ed8aa9e37bb60cf79acf50db03cd888902e7878
shreesh-pandey <shree25g@gmail.com>
2025-07-27 15:55:04 +0530

    Solve Q.8: Print all even numbers between 1 and 100.

---
+++ Chapter-7/PracticeSet/7.8 Problem8.py
@@ -0,0 +1,11 @@
+# Q.8: Print all even numbers between 1 and 100.
+
+print("Even numbers from 1 to 100 are:")
+for i in range (1, 101):
+    if i%2 == 0:
+        print(f"{i}")
+
+
+# -------------------------- -------------------------- -------------------------- #
+
+
